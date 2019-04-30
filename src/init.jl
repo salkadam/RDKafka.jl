9d13a330e72aa75298f1f58c8e1581b12d3d1a6c
salkadam <salkadam@gmail.com>
2019-04-30 11:19:50 -0700

    assuming the .so is in deps directory on root of project

--- src/init.jl
+++ src/init.jl
@@ -2,7 +2,7 @@
 DIR = @__DIR__
 
 if DIR != nothing
-    const LIBRDKAFKA = DIR * "/../deps/librdkafka.so"
+    const LIBRDKAFKA = DIR * "deps/librdkafka.so"
 else
     const LIBRDKAFKA = Pkg.dir("RDKafka", "deps", "librdkafka.so")
 end
